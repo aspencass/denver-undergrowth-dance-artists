264429f0e624154ed872f479ea8543b94dd1945d
aspencass <caspenw1@gmail.com>
2022-11-14 15:56:11 -0700

    more twiggling

--- client/src/components/ResourceForm.js
+++ client/src/components/ResourceForm.js
@@ -5,39 +5,22 @@ function ResourceForm({ addResource }) {
   // resource form data
 
   const [resourceFormData, setResourceFormData] = useState({
-    name: "",
     title: "",
     description: "",
     date: "",
     image: "",
     location_name: "",
     address: "",
-    city: "",
-    state: "",
-    zip: "",
+    email: "",
+    website: "",
+    social: "",
   });
-
-  //   contact form data
-
-  // const [contactFormData, setContactFormData] = useState({
-  //     name: "",
-  //     email: "",
-  //     phone: "",
-  //     website: "",
-  //     social: "",
-  // });
-
-  // category form data
-
-  // const [categoryFormData, setCategoryFormData] = useState({
-  //     name: "",
-  // });
 
   //   errors
   const [errors, setErrors] = useState([]);
 
   //   resource post
-  const handleResourceChange = (e) => {
+  const handleChange = (e) => {
     const { name, value } = e.target;
     setResourceFormData({ ...resourceFormData, [name]: value });
   };
@@ -63,80 +46,18 @@ function ResourceForm({ addResource }) {
     });
   }
 
-  // category post
-  // const handleCategoryChange = (e) => {
-  //     const { name, value } = e.target;
-  //     setCategoryFormData({ ...categoryFormData, [name]: value });
-  // };
-
-  // function onCategorySubmit(e) {
-  //     e.preventDefault();
-
-  //     fetch("/categories", {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/json" },
-  //     body: JSON.stringify({ ...categoryFormData, ongoing: true }),
-  //     }).then((res) => {
-  //     res.json().then(addCategory);
-  //     });
-  // }
-
-  // contact post
-  // const handleContactChange = (e) => {
-  //     const { name, value } = e.target;
-  //     setContactFormData({ ...contactFormData, [name]: value });
-  // };
-
-  // function onContactSubmit(e) {
-  //     e.preventDefault();
-
-  //     fetch("/contacts", {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/json" },
-  //     body: JSON.stringify({ ...contactFormData, ongoing: true }),
-  //     }).then((res) => {
-  //     res.json().then(addContact);
-  //     });
-  // }
 
   return (
-    <div className="gap-6 flex flex-col items-center bg-[url('./assets/nude.png')] bg-cover h-max w-screen">
+    <div className="gap-6 flex flex-col items-center bg-[url('./assets/nude.png')] bg-cover h-max w-screen pb-48">
       <h1 class="text-center text-6xl font-extrabold w-50 tracking-wide">
         Add your class, performance, workshop, funding opportunity, audition,
         rehearsal space, <br />
         Denver Dance Resource here!
       </h1>
-      {/* category submit form  */}
-      {/* <form onSubmit={onCategorySubmit}>
-                <div className="font-bold gap-10 flex flex-row items-center mt-6">
-                <div className="mb-2 block">
-                    <Label htmlFor="name" value="Resource Type:" />
-                </div>
-                <TextInput
-                    type="text"
-                    name="name"
-                    value={categoryFormData.name}
-                    onChange={handleCategoryChange}
-                />
-                </div>
-            </form> */}
 
       {/* resource submit form  */}
       {errors ? errors.map((e) => <div>{e}</div>) : null}
       <form onSubmit={onResourceSubmit}>
-        {/* <div className="font-bold gap-10 flex flex-row items-center mt-6">
-          <div className="mb-2 block">
-            <Label htmlFor="name" value="Resource Type:" />
-          </div>
-          <TextInput
-            type="text"
-            name="name"
-            key={resourceFormData.category.id}
-            value={resourceFormData.category.name}
-            onChange={handleResourceChange}
-          />
-        </div> */}
-        
         <div className="font-bold gap-10 flex flex-row items-center mt-6">
           <div className="mb-2 block">
             <Label htmlFor="title" value="Resource Heading:" />
@@ -145,7 +66,7 @@ function ResourceForm({ addResource }) {
             type="text"
             name="title"
             value={resourceFormData.title}
-            onChange={handleResourceChange}
+            onChange={handleChange}
           />
         </div>
         <div className="font-bold gap-10 flex flex-row items-center mt-6">
@@ -156,7 +77,7 @@ function ResourceForm({ addResource }) {
             type="text"
             name="description"
             value={resourceFormData.description}
-            onChange={handleResourceChange}
+            onChange={handleChange}
           />
         </div>
         <div className="font-bold gap-10 flex flex-row items-center mt-6">
@@ -167,7 +88,7 @@ function ResourceForm({ addResource }) {
             type="number"
             name="date"
             value={resourceFormData.date}
-            onChange={handleResourceChange}
+            onChange={handleChange}
           />
         </div>
         <div className="font-bold gap-10 flex flex-row items-center mt-6">
@@ -178,7 +99,7 @@ function ResourceForm({ addResource }) {
             type="text"
             name="image"
             value={resourceFormData.image}
-            onChange={handleResourceChange}
+            onChange={handleChange}
           />
         </div>
         <div className="font-bold gap-10 flex flex-row items-center mt-6">
@@ -189,7 +110,7 @@ function ResourceForm({ addResource }) {
             type="text"
             name="location_name"
             value={resourceFormData.location_name}
-            onChange={handleResourceChange}
+            onChange={handleChange}
           />
         </div>
         <div className="font-bold gap-10 flex flex-row items-center mt-6">
@@ -200,40 +121,40 @@ function ResourceForm({ addResource }) {
             type="text"
             name="address"
             value={resourceFormData.address}
-            onChange={handleResourceChange}
+            onChange={handleChange}
           />
         </div>
         <div className="font-bold gap-10 flex flex-row items-center mt-6">
           <div className="mb-2 block">
-            <Label htmlFor="city" value="City:" />
+            <Label htmlFor="email" value="Email:" />
           </div>
           <TextInput
             type="text"
-            name="city"
-            value={resourceFormData.city}
-            onChange={handleResourceChange}
+            name="email"
+            value={resourceFormData.email}
+            onChange={handleChange}
           />
         </div>
         <div className="font-bold gap-10 flex flex-row items-center mt-6">
           <div className="mb-2 block">
-            <Label htmlFor="state" value="State:" />
+            <Label htmlFor="website" value="Website:" />
           </div>
           <TextInput
             type="text"
-            name="state"
-            value={resourceFormData.state}
-            onChange={handleResourceChange}
+            name="website"
+            value={resourceFormData.website}
+            onChange={handleChange}
           />
         </div>
         <div className="font-bold gap-10 flex flex-row items-center mt-6">
           <div className="mb-2 block">
-            <Label htmlFor="zip" value="Zip:" />
+            <Label htmlFor="social" value="Social:" />
           </div>
           <TextInput
-            type="number"
-            name="zip"
-            value={resourceFormData.zip}
-            onChange={handleResourceChange}
+            type="text"
+            name="social"
+            value={resourceFormData.social}
+            onChange={handleChange}
           />
         </div>
         <Button
@@ -242,76 +163,12 @@ function ResourceForm({ addResource }) {
         >
           Add Resource
         </Button>
+
       </form>
+
       {errors
         ? errors.map((e) => <h2 style={{ color: "red" }}>{e.toUpperCase()}</h2>)
         : null}
-
-      {/* contact submit form  */}
-      {/* <form onSubmit={onContactSubmit}>
-                <div class="gap-6 flex flex-col items-center bg-[url('./assets/nude.png')] bg-cover h-screen w-screen">
-                <h1 class="text-6xl font-extrabold w-50">Contact Information:</h1>
-                <div className="font-bold gap-10 flex flex-row items-center mt-6">
-                    <div className="mb-2 block">
-                    <Label htmlFor="name" value="Name:" />
-                    </div>
-                    <TextInput
-                    type="text"
-                    name="name"
-                    value={contactFormData.name}
-                    onChange={handleContactChange}
-                    />
-                </div>
-
-                <div className="font-bold gap-10 flex flex-row items-center mt-6">
-                    <div className="mb-2 block">
-                    <Label htmlFor="email" value="Email:" />
-                    </div>
-                    <TextInput
-                    type="text"
-                    name="email"
-                    value={contactFormData.email}
-                    onChange={handleContactChange}
-                    />
-                </div>
-
-                <div className="font-bold gap-10 flex flex-row items-center mt-6">
-                    <div className="mb-2 block">
-                    <Label htmlFor="phone" value="Phone:" />
-                    </div>
-                    <TextInput
-                    type="number"
-                    name="phone"
-                    value={contactFormData.phone}
-                    onChange={handleContactChange}
-                    />
-                </div>
-
-                <div className="font-bold gap-10 flex flex-row items-center mt-6">
-                    <div className="mb-2 block">
-                    <Label htmlFor="website" value="Website:" />
-                    </div>
-                    <TextInput
-                    type="text"
-                    name="website"
-                    value={contactFormData.website}
-                    onChange={handleContactChange}
-                    />
-                </div>
-
-                <div className="font-bold gap-10 flex flex-row items-center mt-6">
-                    <div className="mb-2 block">
-                    <Label htmlFor="social" value="Social:" />
-                    </div>
-                    <TextInput
-                    type="text"
-                    name="social"
-                    value={contactFormData.social}
-                    onChange={handleContactChange}
-                    />
-                </div> */}
-      {/* </div> */}
-      {/* </form> */}
     </div>
   );
 }
