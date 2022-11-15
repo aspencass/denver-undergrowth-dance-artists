e85fd18fadf146ae6559625d4d55fa7790b23621
aspencass <caspenw1@gmail.com>
2022-11-15 15:02:04 -0700

    user page css fix and login works

--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -1,38 +1,38 @@
-import { useEffect, useState } from 'react'
-import {useParams} from 'react-router-dom'
-import Home from './Home';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Home from "./Home";
 
-function UserPage(){
-    const [user, setUser] = useState()
-    const [loading, setLoading] = useState(true)
-    const [errors, setErrors] = useState(false)
-    
-    const params = useParams()
-    const {id} = params
-    
-    useEffect(()=>{
-        fetch(`/users/${id}`)
-        .then(res => {
-            if(res.ok){
-                res.json().then(user => {
-                    setUser(user)
-                    setLoading(false)
-                })
-            }else {
-                res.json().then(data => setErrors(data.errors))
-            }
-        })
-       
-    },[])
+function UserPage() {
+  const [user, setUser] = useState();
+  const [loading, setLoading] = useState(true);
+  const [errors, setErrors] = useState(false);
 
-    if(loading) return <h1>Loading</h1>
-    if(errors) return <h1>{errors}</h1>
-    return (
-        <div>
-            <h1>Hello, {user.username}</h1>
-            <Home /> 
-        </div>
-    )
+  const params = useParams();
+  const { id } = params;
+
+  useEffect(() => {
+    fetch(`/users/${id}`).then((res) => {
+      if (res.ok) {
+        res.json().then((user) => {
+          setUser(user);
+          setLoading(false);
+        });
+      } else {
+        res.json().then((data) => setErrors(data.errors));
+      }
+    });
+  }, []);
+
+  if (loading) return <h1>Loading</h1>;
+  if (errors) return <h1>{errors}</h1>;
+  return (
+    <div>
+      <h1 class="text-center text-8xl font-extrabold w-50 tracking-wide p-20 bg-[url('./assets/nude.png')] bg-cover">
+        hello {user.username}!
+      </h1>
+      <Home />
+    </div>
+  );
 }
 
-export default UserPage
+export default UserPage;
