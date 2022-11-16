a67a32c02f0d00644f94fdeb95f71279b2a41e30
aspencass <caspenw1@gmail.com>
2022-11-15 21:58:44 -0700

    search bar implemented

--- client/src/components/ResourcePage.js
+++ client/src/components/ResourcePage.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ResourceCard from "./ResourceCard";
+import Search from "./Search";
 
 function ResourcePage({ resources }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const displayedResources = resources.filter(resource => {
+    return resource.description.toLowerCase().includes(searchTerm.toLowerCase());
+   })
+
   return (
     <>
+    <Search searchTerm={searchTerm} onSearchChange={setSearchTerm}/>
       <div className="bg-[url('./assets/nude.png')] bg-cover bg-repeat">
         <div className="justify-center content-start grid grid-row gap-10">
-          {resources.map((resource) => (
+          {displayedResources.map((resource) => (
             <ResourceCard
               key={resource.id}
               resource={resource}
---
+++ client/src/components/Search.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Label, TextInput } from "flowbite-react";
+
+function Search({ searchTerm, onSearchChange }) {
+  return (
+    <>
+      <div className="bg-[url('./assets/nude.png')] bg-cover">
+        <div className="searchbar">
+          <label
+            htmlFor="search"
+            className="text-8xl text-center font-bold tracking-tight text-gray-900 pb-6"
+          >
+            Search Resources:
+          </label>
+          <input
+            type="text"
+            id="search"
+            placeholder="Type to search..."
+            value={searchTerm}
+            onChange={(e) => onSearchChange(e.target.value)}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Search;
